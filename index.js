07e89ce3c5d51bc8f1110894f111293b21d3ad5c
Роман Казаков <roman.delarge@gmail.com>
2023-08-22 20:19:24 +0300

    added some functionality

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
-const CoinGecko = require('coingecko-api');
+const  CoinGecko  = require('coingecko-api');
+const { osmosis } = require('osmojs')
 
 const config = require('./config/default.json')
 
-const bot = new Telegraf(config.token);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.on(message('sticker'), (ctx) => {
-        bitcoin_price().then(res => ctx.reply(`Цена битка сейчас ${res.data.bitcoin.usd} баксов`))
-    });
-bot.launch();
-
 const CoinGeckoClient = new CoinGecko();
+const REST_ENDPOINT = 'https://lcd.osmosis.zone';
 
-var bitcoin_price = async() => {
-    let data = await CoinGeckoClient.simple.price({
-                                                    ids: 'bitcoin',
+const prices = async() => {
+    let pricesData = await CoinGeckoClient.simple.price({
+                                                    ids: ['cosmos', 'juno-network'], 
                                                     vs_currencies: 'usd',
                                                     });
+                                                    
+    const client = await osmosis.ClientFactory.createLCDClient({ restEndpoint: REST_ENDPOINT });
+    const phmnatomPool = await client.osmosis.gamm.v1beta1.pool({poolId: '867'})
+    const atomAmountInphmnatomPool = phmnatomPool.pool.pool_assets[0].token.amount
+    const phmnAmountInphmnatomPool = phmnatomPool.pool.pool_assets[1].token.amount
+    const phmnatomPrice = atomAmountInphmnatomPool / phmnAmountInphmnatomPool
+    const atomusdPrice = pricesData.data.cosmos.usd
+    const phmnatomPriceusd = atomAmountInphmnatomPool / phmnAmountInphmnatomPool * atomusdPrice
 
-    return data
-}
+    const junousdPrice = pricesData.data['juno-network'].usd
 
+    const phmnibcxPool = await client.osmosis.gamm.v1beta1.pool({poolId: '1042'})
+    const ibcxAmountInphmnatomPool = phmnatomPool.pool.pool_assets[0].token.amount
+    const phmnAmountInphmnibcxPool = phmnatomPool.pool.pool_assets[1].token.amount
+    const phmnibcxPrice = atomAmountInphmnatomPool / phmnAmountInphmnatomPool
 
+    return [atomusdPrice, junousdPrice, phmnatomPriceusd, phmnatomPrice, phmnibcxPrice]
+} 
+
+const bot = new Telegraf(config.token);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.command('phmn', (ctx) => {
+        prices().then(data => ctx.replyWithHTML(`
+<strong>ATOM price:</strong> ${data[0].toFixed(2)}$
+<strong>JUNO price:</strong> ${data[1].toFixed(2)}$
+<strong>PHMN-ATOM price:</strong> ${data[2].toFixed(2)}$
+1 $PHMN  = ${data[3].toFixed(2)} $ATOM
+1 $PHMN  = ${data[4].toFixed(2)} $IBCX`
+    ));
+});
+bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
